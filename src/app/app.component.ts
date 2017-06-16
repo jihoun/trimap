7aab09ce906e4aa5ce1b25ff1e5a2ad8617960de
Nicolas Lagier <nicolas.lagier@giift.com>
2017-06-16 17:17:20 -0400

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+import {Race} from './race.model';
+import {EventsService} from './events.service';
+import {GeocodeService} from "./geocode.service";
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['app.component.css'],
+  templateUrl: 'app.component.html',
+  providers: [EventsService, GeocodeService]
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+
+  lat: number = 51.678418;
+  lng: number = 7.809007;
+
+  races: Array<Race>;
+  fancyRaces: Array<Race> = [];
+
+  errorMessage: String = '';
+
+  constructor (private eventsService: EventsService, private geocode: GeocodeService) {}
+
+  ngOnInit(): void {
+    this.eventsService.getEvents().subscribe(
+        events => { this.races = events; this.fetchLocations()},
+        error => this.errorMessage = <any>error
+    );
+  }
+
+  fetchLocations(): void {
+    for (let race of this.races) {
+      this.geocode.getLatLon(race.address, race.country)
+          .subscribe(
+              latLng => {race.lat = latLng.lat(); race.lon = latLng.lng(); this.fancyRaces.push(race);}
+          );
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {HttpModule, JsonpModule} from '@angular/http';
 
-import { AppComponent } from './app.component';
+import {AgmCoreModule} from '@agm/core';
+
+import {AppComponent} from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        HttpModule,
+        AgmCoreModule.forRoot({
+            apiKey: 'AIzaSyDa_fM9YGg9oVMc6nzBw5_5xiI2Ag4CXOA'
+        }),
+        JsonpModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
